853d5b3baa244c2e6890d4fd12125eb7cc088740
wenpeifang <wenpeifang@mac-top.mobi>
2014-01-09 11:57:51 +0800

    [Finishes #63097766]

--- teacherPal/SchoolBusPhoto/SchoolBusPhoto/Student/GKClassViewController.h
+++ teacherPal/SchoolBusPhoto/SchoolBusPhoto/Student/GKClassViewController.h
@@ -5,7 +5,9 @@
 //  Created by wen peifang on 14-1-3.
 //  Copyright (c) 2014年 mactop. All rights reserved.
 //
-
+/*
+ 展示 班级积分 上传列表 班级学生 功能
+ */
 #import <UIKit/UIKit.h>
 #import "GKBaseViewController.h"
 @interface GKClassViewController : GKBaseViewController<UITableViewDelegate,UITableViewDataSource>
